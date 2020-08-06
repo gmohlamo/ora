2fce2f709dd120bf9d38e82312cbe19ce29eb3b0
Gladwin Mohlamonyane <g.mohlamonyane@gmail.com>
2020-08-06 08:09:44 -0400

    changed it a bit

--- dbDump.go
+++ dbDump.go
@@ -10,7 +10,7 @@ import (
 	"github.com/k0kubun/pp"
 )
 
-var f *os.File = os.Stdout
+//var f *os.File = os.Stdout
 
 //Currently there seems to be an issue with getting the output
 //of this script into a file; however, it works well on the terminal
@@ -20,7 +20,7 @@ var f *os.File = os.Stdout
 //perhaps a file descriptor would prove to be more effective
 func main() {
 	//create file
-	//f, err := os.OpenFile("oracleDB.log", os.O_WRONLY|os.O_CREATE, 0644)
+	f, err := os.OpenFile("oracleDB.audit", os.O_WRONLY|os.O_CREATE, 0644)
 	//gracefulCrash(err, true)
 	defer f.Close()
 	//println(len(os.Args))
@@ -36,46 +36,90 @@ func main() {
 	gracefulCrash(err, true)
 	defer db.Close()
 
-	userSysPrivsRows, err := db.Query("SELECT * FROM USER_SYS_PRIVS")
+	fmt.Fprintf(f, "Ensure All Default Passwords Are Changed\n")
+	rows, err := db.Query("SELECT USERNAME FROM DBA_USERS_WITH_DEFPWD WHERE USERNAME NOT LIKE '%XS$NULL%'")
 	gracefulCrash(err, true)
-	color.HiRed("\nUser System Privs:\n")
-	color.Green("=====================================================>")
-	prettyPrintQuery(userSysPrivsRows)
-	//databases on database
-	//commented out for brevity, but could prove to provide valuable information
-	/*
-		databaseRows, err := db.Query("SELECT * FROM v$database")
-		gracefulCrash(err)
-		color.HiRed("\nAll databases:\n")
-		color.Green("=====================================================>")
-		prettyPrintQuery(databaseRows)
-	*/
-	//tables
-	tables, err := db.Query("SELECT table_name FROM all_tables ORDER BY table_name")
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure All Sample Data And Users Have Been Removed\n")
+	rows, err = db.Query("SELECT USERNAME FROM ALL_USERS WHERE USERNAME IN ('BI','HR','IX','OE','PM','SCOTT','SH')")
 	gracefulCrash(err, true)
-	fmt.Fprintf(f, "%s", color.HiRedString("\nAll tables in the currently mounted database:\n"))
-	fmt.Fprintf(f, "%s", color.GreenString("=====================================================>"))
-	tableNames := prettyPrintTableQuery(tables)
-	tableNamesStr := pp.Sprint(tableNames)
-	fmt.Fprintf(f, "%s", tableNamesStr)
-	fmt.Fprintf(f, "%s", color.RedString("\nEach tables contents"))
-	fmt.Fprintf(f, "%s", color.GreenString("=====================================================>"))
-	for _, name := range tableNames {
-		query := fmt.Sprintf("SELECT * FROM %s FETCH NEXT %d ROWS ONLY", name, 5)
-		table, err := db.Query(query)
-		skip := gracefulCrash(err, false)
-		if skip != false {
-			continue
-		}
-		fmt.Fprintf(f, "%s", color.CyanString("=====================================================>"))
-		tabName := color.RedString(name)
-		fmt.Fprintf(f, "Contents of table %s\n", tabName)
-		prettyPrintQuery(table)
-	}
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'AUDIT_SYS_OPERATIONS' Is Set to 'TRUE'\n")
+	rows, err = db.Query("SELECT UPPER(VALUE) FROM V$PARAMETER WHERE UPPER(NAME) = 'AUDIT_SYS_OPERATIONS'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'AUDIT_TRAIL' Is Set\n")
+	rows, err = db.Query("SELECT UPPER(VALUE) FROM V$PARAMETER WHERE UPPER(NAME)='AUDIT_TRAIL'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'GLOBAL_NAMES' Is Set to 'TRUE'\n")
+	rows, err = db.Query("SELECT UPPER(VALUE) FROM V$PARAMETER WHERE UPPER(NAME)='GLOBAL_NAMES'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'LOCAL_LISTENER' Is Set\n")
+	rows, err = db.Query("SELECT UPPER(VALUE) FROM V$PARAMETER WHERE UPPER(NAME)='LOCAL_LISTENER'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'OS_ROLES' Is Set to 'FALSE'\n")
+	rows, err = db.Query("SELECT UPPER(VALUE) FROM V$PARAMETER WHERE UPPER(NAME)='OS_ROLES'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'REMOTE_OS_ROLES' Is Set to 'FALSE'\n")
+	rows, err = db.Query("SELECT UPPER(VALUE) FROM V$PARAMETER WHERE UPPER(NAME)='REMOTE_OS_ROLES'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'SEC_CASE_SENSITIVE_LOGON' Is Set to 'TRUE'\n")
+	rows, err = db.Query("SELECT UPPER(VALUE) FROM V$PARAMETER WHERE UPPER(NAME)='SEC_CASE_SENSITIVE_LOGON'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'FAILED_LOGIN_ATTEMPTS' Is Less than or Equal to '5'\n")
+	rows, err = db.Query("SELECT PROFILE, RESOURCE_NAME, LIMIT FROM DBA_PROFILES WHERE RESOURCE_NAME='FAILED_LOGIN_ATTEMPTS' AND (LIMIT = 'DEFAULT' OR LIMIT = 'UNLIMITED' OR LIMIT > 5)")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'PASSWORD_LIFE_TIME' Is Less than or Equal to '90'\n")
+	rows, err = db.Query("SELECT PROFILE, RESOURCE_NAME, LIMIT FROM DBA_PROFILES WHERE RESOURCE_NAME='PASSWORD_LIFE_TIME' AND (LIMIT = 'DEFAULT' OR LIMIT = 'UNLIMITED' OR LIMIT > 90)")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'SESSIONS_PER_USER' Is Less than or Equal to '10'\n")
+	rows, err = db.Query("SELECT PROFILE, RESOURCE_NAME, LIMIT FROM DBA_PROFILES WHERE RESOURCE_NAME='SESSIONS_PER_USER' AND (LIMIT = 'DEFAULT' OR LIMIT = 'UNLIMITED' OR LIMIT > 10)")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'EXECUTE' Is Revoked from 'PUBLIC' on 'DBMS_ADVISOR'\n")
+	rows, err = db.Query("SELECT PRIVILEGE FROM DBA_TAB_PRIVS WHERE GRANTEE='PUBLIC' AND PRIVILEGE='EXECUTE' AND TABLE_NAME='DBMS_ADVISOR'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'EXECUTE' Is Revoked from 'PUBLIC' on 'DBMS_JAVA'\n")
+	rows, err = db.Query("SELECT PRIVILEGE FROM DBA_TAB_PRIVS WHERE GRANTEE='PUBLIC' AND PRIVILEGE='EXECUTE' AND TABLE_NAME='DBMS_JAVA'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'EXECUTE' Is Revoked from 'PUBLIC' on 'DBMS_JAVA_TEST'\n")
+	rows, err = db.Query("SELECT PRIVILEGE FROM DBA_TAB_PRIVS WHERE GRANTEE='PUBLIC' AND PRIVILEGE='EXECUTE' AND TABLE_NAME='DBMS_JAVA_TEST'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'EXECUTE' Is Revoked from 'PUBLIC' on 'DBMS_BACKUP_RESTORE'\n")
+	rows, err = db.Query("SELECT PRIVILEGE FROM DBA_TAB_PRIVS WHERE GRANTEE='PUBLIC' AND PRIVILEGE='EXECUTE' AND TABLE_NAME='DBMS_BACKUP_RESTORE'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Ensure 'DBA' Is Revoked from Unauthorized 'GRANTEE'\n")
+	rows, err = db.Query("SELECT GRANTEE, GRANTED_ROLE FROM DBA_ROLE_PRIVS WHERE GRANTED_ROLE='DBA' AND GRANTEE NOT IN ('SYS','SYSTEM')")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Enable 'USER' Audit Option\n")
+	rows, err = db.Query("SELECT AUDIT_OPTION, SUCCESS, FAILURE FROM DBA_STMT_AUDIT_OPTS WHERE AUDIT_OPTION='USER' AND USER_NAME IS NULL AND PROXY_NAME IS NULL AND SUCCESS = 'BY ACCESS' AND FAILURE = 'BY ACCESS'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Enable 'ALTER	USER' Audit Option\n")
+	rows, err = db.Query("SELECT AUDIT_OPTION, SUCCESS, FAILURE FROM DBA_STMT_AUDIT_OPTS WHERE AUDIT_OPTION='ALTER USER' AND USER_NAME IS NULL AND PROXY_NAME IS NULL AND SUCCESS = 'BY ACCESS' AND FAILURE = 'BY ACCESS'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
+	fmt.Fprintf(f, "Enable 'DROP USER' Audit Option\n")
+	rows, err = db.Query("SELECT AUDIT_OPTION, SUCCESS, FAILURE FROM DBA_STMT_AUDIT_OPTS WHERE AUDIT_OPTION='DROP USER' AND USER_NAME IS NULL AND PROXY_NAME IS NULL AND SUCCESS = 'BY ACCESS' AND FAILURE = 'BY ACCESS'")
+	gracefulCrash(err, true)
+	prettyPrintQuery(rows, f)
 	color.Red("Script Completed Execution\n")
 }
 
-func prettyPrintQuery(rows *sql.Rows) {
+func prettyPrintQuery(rows *sql.Rows, f *os.File) {
 	dbColumns, err := rows.Columns()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
@@ -108,11 +152,11 @@ func prettyPrintQuery(rows *sql.Rows) {
 		fmt.Fprintf(f, "%s", color.CyanString("=====================================================>"))
 		//Dump to output
 		str := pp.Sprint(m)
-		fmt.Fprintf(f, "%s", str)
+		fmt.Fprintf(f, "%s\n", str)
 	}
 }
 
-func prettyPrintTableQuery(rows *sql.Rows) []string {
+func prettyPrintTableQuery(rows *sql.Rows, f *os.File) []string {
 	dbColumns, err := rows.Columns()
 	var tables []string
 	if err != nil {
@@ -149,7 +193,7 @@ func prettyPrintTableQuery(rows *sql.Rows) []string {
 		fmt.Fprintf(f, "%s", color.CyanString("=====================================================>"))
 		//Dump to output
 		str := pp.Sprint(m)
-		fmt.Fprintf(f, "%s", str)
+		fmt.Fprintf(f, "%s\n", str)
 	}
 	return tables
 }
